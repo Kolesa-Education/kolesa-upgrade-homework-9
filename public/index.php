7e414ffed969063bdab09876c48b744bc69f4ea6
Shalqarov <shalqarov01@gmail.com>
2022-11-02 01:38:09 +0600

    add new route

--- public/index.php
+++ public/index.php
@@ -2,25 +2,54 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\Config;
 use App\Http\Controllers;
+use App\Repository\AdvertRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 use Twig\Error\LoaderError;
+use DI\Container;
+use function DI\create;
 
 try {
     $twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
 } catch (LoaderError $e) {
     die(500);
 }
+
+$params = [
+    'host' => $_ENV['DB_HOST'],
+    'user' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'dbname' => $_ENV['DB_DATABASE'],
+    'driver' => $_ENV['DB_DRIVER'] ?? 'pdo_mysql',
+];
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$container = new Container();
+$container->set(Config::class, create(Config::class)->constructor($_ENV));
+$container->set(EntityManager::class, fn(Config $config) => EntityManager::create(
+    $config->db,
+    ORMSetup::createAttributeMetadataConfiguration([__DIR__ . '/../src/Model'])
+));
+
+
+AppFactory::setContainer($container);
+
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 $app->add(TwigMiddleware::create($app, $twig));
 
 $app->get('/', Controllers\IndexController::class . ':home');
-$app->get('/item/{id}', Controllers\AdvertController::class . ':getItem');
 $app->get('/adverts', Controllers\AdvertController::class . ':index');
 $app->get('/adverts/new', Controllers\AdvertController::class . ':newAdvert');
 $app->post('/adverts', Controllers\AdvertController::class . ':create');
+$app->get('/adverts/{id}', Controllers\AdvertController::class . ':getItem');
 
 $app->run();
