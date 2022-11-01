94410c35a29e9994aabf838ced4b34f4ea5d7481
tshalkarov <tshalkarov@kaptechnology.kazatomprom.kz>
2022-11-01 22:14:21 +0600

    add new route to getItem

--- public/index.php
+++ public/index.php
@@ -6,13 +6,19 @@ use App\Http\Controllers;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
+use Twig\Error\LoaderError;
 
-$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
-$app  = AppFactory::create();
+try {
+    $twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+} catch (LoaderError $e) {
+    die(500);
+}
+$app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 $app->add(TwigMiddleware::create($app, $twig));
 
 $app->get('/', Controllers\IndexController::class . ':home');
+$app->get('/item/{id}', Controllers\AdvertController::class . ':getItem');
 $app->get('/adverts', Controllers\AdvertController::class . ':index');
 $app->get('/adverts/new', Controllers\AdvertController::class . ':newAdvert');
 $app->post('/adverts', Controllers\AdvertController::class . ':create');
