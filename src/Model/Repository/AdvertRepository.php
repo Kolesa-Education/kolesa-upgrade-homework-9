36bc4ff2a47c6785c5199e3458eaff7f573ec288
UlanZhassanov <ulanzhasanov@gmail.com>
2022-11-02 07:58:07 +0600

    3 задачи выполнены

--- src/Model/Repository/AdvertRepository.php
+++ src/Model/Repository/AdvertRepository.php
@@ -6,7 +6,11 @@ use App\Model\Entity\Advert;
 
 class AdvertRepository
 {
-    private const DB_PATH = '../storage/adverts.json';
+    // private const DB_PATH = '../storage/adverts.json'; 
+    private const dbHost = 'localhost';
+    private const dbUser = 'root';
+    private const dbPass = '';
+    private const dbName = 'adverts_homework9';
 
     public function getAll()
     {
@@ -23,39 +27,87 @@ class AdvertRepository
     {
         $db = $this->getDB();
         $result = [];
-        $result[] = new Advert($db[$id]);
-        // print_r($result[0]);
+        $result[] = new Advert($db[$id-1]);
 
         return $result[0];
     }
 
-    public function create(array $advertData): Advert {
-        $db               = $this->getDB();
-        $increment        = array_key_last($db) + 1;
-        $advertData['id'] = $increment;
-        $db[$increment]   = $advertData;
-
-        $this->saveDB($db);
+    public function create(array $advertData): Advert
+    {
+        $this->saveDB($advertData);
 
         return new Advert($advertData);
     }
 
-    public function edit(array $advertData): Advert {
-        $db               = $this->getDB();
-        $db[$advertData['id']]   = $advertData;
-
-        $this->saveDB($db);
-
+    public function edit(array $advertData): Advert
+    {
+        $this->updateDB($advertData);
         return new Advert($advertData);
     }
 
     private function getDB(): array
     {
-        return json_decode(file_get_contents(self::DB_PATH), true) ?? [];
+        // return json_decode(file_get_contents(self::DB_PATH), true) ?? [];
+        $conn = mysqli_connect(self::dbHost, self::dbUser, self::dbPass, self::dbName);
+        if ($conn == false) {
+            print(mysqli_connect_error());
+            return [];
+        }
+        mysqli_set_charset($conn, "utf8");
+
+        $sql = 'SELECT `id`, `title`, `description`, `price` FROM `adverts`';
+        $result = mysqli_query($conn, $sql);
+        $allAdverts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        return $allAdverts ?? [];
     }
 
-    private function saveDB(array $data):void
+    private function saveDB(array $data): void
     {
-        file_put_contents(self::DB_PATH, json_encode($data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+        // file_put_contents(self::DB_PATH, json_encode($data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+        $conn = mysqli_connect(self::dbHost, self::dbUser, self::dbPass, self::dbName);
+        if ($conn == false) {
+            print(mysqli_connect_error());
+            return;
+        } else {
+            print("success");
+        }
+        mysqli_set_charset($conn, "utf8");
+
+        $title = $data['title'];
+        $description = $data['description'];
+        $price = $data['price'];
+        print_r($data['description'], $data['price']);
+        $sql = 'INSERT INTO adverts(title, description, price) VALUES ("' . $title . '", "' . $description . '", ' . $price.')';
+
+        $result = mysqli_query($conn, $sql);
+        if ($result == false) {
+            print("Произошла ошибка при выполнении запроса");
+        }
+    }
+
+    private function updateDB(array $data): void
+    {
+        // file_put_contents(self::DB_PATH, json_encode($data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+        $conn = mysqli_connect(self::dbHost, self::dbUser, self::dbPass, self::dbName);
+        if ($conn == false) {
+            print(mysqli_connect_error());
+            return;
+        } else {
+            print("success");
+        }
+        mysqli_set_charset($conn, "utf8");
+
+        $id = $data['id'];
+        $title = $data['title'];
+        $description = $data['description'];
+        $price = $data['price'];
+        print_r($data['description'], $data['price']);
+        $sql = 'UPDATE adverts SET title = "' . $title . '", description = "' . $description . '", price = ' . $price.' WHERE id = '. $id .';';
+
+        $result = mysqli_query($conn, $sql);
+        if ($result == false) {
+            print("Произошла ошибка при выполнении запроса");
+        }
     }
 }
