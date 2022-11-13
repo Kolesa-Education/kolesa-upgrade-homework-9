711cb87425b7c4ac849bf8254f248a8429186af4
Yevgeniy Vesselovskiy <106279381+vgenkon@users.noreply.github.com>
2022-11-13 12:16:48 +0600

    выполняю задание 9

--- public/index.php
+++ public/index.php
@@ -6,8 +6,12 @@ use App\Http\Controllers;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
+use Twig\Error\LoaderError;
 
-$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+try {
+    $twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+} catch (LoaderError $e) {
+}
 $app  = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 $app->add(TwigMiddleware::create($app, $twig));
@@ -17,8 +21,8 @@ $app->get('/adverts', Controllers\AdvertController::class . ':index');
 $app->get('/adverts/new', Controllers\AdvertController::class . ':newAdvert');
 $app->post('/adverts', Controllers\AdvertController::class . ':create');
 $app->get('/adverts/{id}', Controllers\AdvertController::class . ':getAdvert');
-$app->get('/adverts/{id}/edit', Controllers\AdvertController::class . ':edAdvert');
+$app->get('/adverts/{id}/edit', Controllers\AdvertController::class . ':editAdvert');
 $app->post('/adverts/{id}/edit', Controllers\AdvertController::class . ':editAdvert');
-$app->get('/adverts/{id}/delete', Controllers\AdvertController::class . ':delAdvert');
-$app->post('/adverts/{id}/delete', Controllers\AdvertController::class . ':deleteAdvert');
+$app->get('/adverts/{id}/delete', Controllers\AdvertController::class . ':deleteAdvert');
+$app->delete('/adverts/{id}/delete', Controllers\AdvertController::class . ':deleteAdvert');
 $app->run();
--- src/Model/Repository/AdvertRepository.php
+++ src/Model/Repository/AdvertRepository.php
@@ -3,69 +3,90 @@
 namespace App\Model\Repository;
 
 use App\Model\Entity\Advert;
+use PDO;
 
 class AdvertRepository
 {
-    private const DB_PATH = '../storage/adverts.json';
+    private const DB_PATH = './storage/adverts.json';
+    private static $pdo;
+    private $link;
 
-    public function getAll()
+    public function __construct()
     {
-        $result = [];
+        $this->connect();
+    }
 
-        foreach ($this->getDB() as $advertData) {
-            $result[] = new Advert($advertData);
+    private function connect()
+    {
+        $config = require_once 'config.php';
+        $dsn = 'mysql:host=' . $config['host'] . ';dbname=' . $config['db_name'] . ';charset=' . $config['charset'];
+        $this->link = new PDO($dsn, $config['username'], $config['password']);
+        return $this;
+    }
+
+    public function execute($sql)
+    {
+        $sth = $this->link->prepare($sql);
+        return $sth->excute();
+    }
+
+    public function query($sql)
+    {
+        $sth = $this->link->prepare($sql);
+        $sth->execute();
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+        if ($result === false) {
+            return [];
         }
-
         return $result;
     }
 
-    public function create(array $advertData): Advert {
-        $db               = $this->getDB();
-        $increment        = array_key_last($db) + 1;
-        $advertData['id'] = $increment;
-        $db[$increment]   = $advertData;
-
-        $this->saveDB($db);
-
-        return new Advert($advertData);
-    }
-
-    private function getDB(): array
+    public function getAll(): array
     {
-        return json_decode(file_get_contents(self::DB_PATH), true) ?? [];
-    }
+        $result = [];
+        $stmt = self::$pdo->query('SELECT * FROM adverts');
+        $data = $stmt->fetchAll();
 
-    private function saveDB(array $data):void
-    {
-        file_put_contents(self::DB_PATH, json_encode($data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-    }
-
-public function getById(int $id): Advert {
-        $arr = $this->getDB();
-        for($i = 1; $i<=count($arr); $i++){
-            if ($arr[$i] == null){
-                continue;
-            }
-            if($arr[$i]['id'] == $id){
-                $advertData = $arr[$i];
-                return new Advert($advertData);
-            }
+        foreach ($data as $advertData) {
+            $result[] = new Advert($advertData);
         }
+        return $result;
     }
-    public function editAdvert(array $advertData,$id): Advert {
-        $db = $this->getDB();
-        $advertData['id'] = $id;
-        $db[$id]   = $advertData;
-        $this->saveDB($db);
-        return new Advert($advertData);
 
+    public function create(array $advertData): Advert
+    {
+        $db = $this->getDB();
+        $title = $advertData['title'];
+        $description = $advertData['description'];
+        $price = $advertData['price'];
+        $sql = "INSERT INTO adverts(title, description, price)values (?, ?, ?)";
+        $stmt = $db->prepare($sql)->execute([$title, $description, $price]);
+        return new Advert($advertData);
     }
-    public function deleteAdvert(array $advertData,$id): Advert {
-        $db = $this->getDB();
-        unset($advertData);
-        $db[$id]   = $advertData;
-        $this->saveDB($db);
-        return new Advert($advertData);
 
+    private function getDB()
+    {
+        return self::$pdo;
+    }
+
+    public function edit(array $advertData, int $id)
+    {
+        $db = $this->getDB();
+        $title = $advertData['title'];
+        $description = $advertData['description'];
+        $price = $advertData['price'];
+        $sql = "UPDATE adverts SET title=?, description=?, price=? WHERE id=?";
+        $db->prepare($sql)->execute([$title, $description, $price, $id]);
+        return new Advert($advertData);
+    }
+    public function deleteAdvert(array $advertData, int $id): Advert
+    {
+        $db = $this->getDB();
+        $title = $advertData['title'];
+        $description = $advertData['description'];
+        $price = $advertData['price'];
+        $sql = "DELETE adverts WHERE id=?";
+        $db->prepare($sql)->execute( [$id]);
+        return new Advert($advertData);
     }
 }
